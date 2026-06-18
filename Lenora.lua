2a5f3cc32c72d715fabe27037d161a2512f1c974
Drem999 <mvp20.cool@gmail.com>
2026-06-18 16:00:41 -0500

    Lenora.lua

--- Lenora.lua
+++ Lenora.lua
@@ -1,1 +1,12 @@
+if game.PlaceId == 114234929420007 or game.PlaceId == 135434213652028 or game.PlaceId == 108194354348181 then
+    getgenv().SCRIPT_KEY = nil
+    loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/8c7009a1f73587dfd6f8ae42c1d49daf0892531635bf3c2eb07430c5c4fd02ca/download"))() --bloxstrike
+    print("Loaded Blox Strike")
+--[=[ 
+elseif game.PlaceId == 301549746 or game.PlaceId == 1480424328 or game.PlaceId == 1869597719 then
+    getgenv().SCRIPT_KEY = nil
+    loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/8c7009a1f73587dfd6f8ae42c1d49daf0892531635bf3c2eb07430c5c4fd02ca/download"))() -- counter blox
+    print("Loaded Counter Blox")
+]=]
 
+end -- End
